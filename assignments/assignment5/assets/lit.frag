c74d0aa7fb685b9de85baf175b4a065a073efb8e
Nate Spielman <nate.spielman@mymail.champlain.edu>
2024-04-04 11:21:45 -0400

    Initial set up of assignment 5

---
+++ assignments/assignment5/assets/lit.frag
@@ -0,0 +1,82 @@
+#version 450
+out vec4 FragColor; //The color of this fragment
+
+in Surface{
+	vec3 WorldPos; //Vertex position in world space
+	vec3 WorldNormal; //Vertex normal in world space
+	vec2 TexCoord;
+}fs_in;
+
+struct Light{
+	vec3 LightDirection;
+	vec3 LightColor;
+	vec3 AmbientColor;
+};
+uniform Light _Light;
+
+struct Material{
+	float Ka; //Ambient coefficient (0-1)
+	float Kd; //Diffuse coefficient (0-1)
+	float Ks; //Specular coefficient (0-1)
+	float Shininess; //Affects size of specular highlight
+};
+uniform Material _Material;
+
+uniform sampler2D _MainTex; //2D texture sampler
+uniform sampler2D _ShadowMap;
+uniform vec3 _EyePos;
+in vec4 LightSpacePos;
+uniform float _MinBias;
+uniform float _MaxBias;
+
+float calcShadow(sampler2D shadowMap, vec4 lightSpacePos, float bias){
+	//Homogeneous Clip space to NDC [-w,w] to [-1,1]
+    vec3 sampleCoord = lightSpacePos.xyz / lightSpacePos.w;
+    //Convert from [-1,1] to [0,1]
+    sampleCoord = sampleCoord * 0.5 + 0.5;
+	//Include bias in depth
+	float myDepth = sampleCoord.z - bias; 
+	float shadowMapDepth = texture(shadowMap, sampleCoord.xy).r;
+
+	//PCF 
+	float totalShadow = 0.0;
+	vec2 texelOffset = 1.0 /  textureSize(_ShadowMap,0);
+	for(int y = -1; y <=1; y++){
+		for(int x = -1; x <=1; x++){
+			vec2 uv = sampleCoord.xy + vec2(x * texelOffset.x, y * texelOffset.y);
+			totalShadow += step(texture(_ShadowMap,uv).r,myDepth);
+		}
+	}
+	totalShadow /= 9.0;
+
+	//shadow outside of far plane of frustum stays at 0.0
+	if(sampleCoord.z > 1.0)
+        totalShadow = 0.0;
+
+	return totalShadow;
+}
+
+void main(){
+	//Make sure fragment normal is still length 1 after interpolation.
+	vec3 normal = normalize(fs_in.WorldNormal);
+	//Light is set to point straight down
+	vec3 toLight = -_Light.LightDirection;
+	float diffuseFactor = max(dot(normal,toLight),0.0);
+	//Direction towards eye
+	vec3 toEye = normalize(_EyePos - fs_in.WorldPos);
+	//Blinn-phong uses half angle
+	vec3 h = normalize(toLight + toEye);
+	float specularFactor = pow(max(dot(normal,h),0.0),_Material.Shininess);
+	//Combination of specular and diffuse reflection
+	vec3 lightColor = (_Material.Kd * diffuseFactor + _Material.Ks * specularFactor) * _Light.LightColor;
+
+	//shadow
+	float bias = max(_MaxBias * (1.0 - dot(normal,toLight)),_MinBias);
+	float shadow = calcShadow(_ShadowMap, LightSpacePos, bias);
+	lightColor *= 1.0 - shadow;
+
+	//Add some ambient light
+	lightColor+=_Light.AmbientColor * _Material.Ka;
+	vec3 objectColor = texture(_MainTex,fs_in.TexCoord).rgb;
+	FragColor = vec4(objectColor * lightColor,1.0);
+}
---
+++ assignments/assignment5/main.cpp
@@ -0,0 +1,253 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/transform.h>
+#include <ew/cameraController.h>
+#include <ew/texture.h>
+#include <ew/procGen.h>
+#include <ns/framebuffer.h>
+#include <ns/shadowMap.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
+
+//Global state
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
+
+//Camera
+ew::Camera camera;
+ew::CameraController cameraController;
+ew::Camera shadowCamera;
+
+//Shadow Map Variables
+int shadowMapWidth = 2048;
+int shadowMapHeight = 2048;
+ns::ShadowMap shadowMap;
+
+struct Material {
+	float Ka = 1.0;
+	float Kd = 0.5;
+	float Ks = 0.5;
+	float Shininess = 128;
+}material;
+
+struct Light {
+	glm::vec3 lightDirection = glm::vec3(0.0f, -1.0f, 0.0f); //Light pointing straight down
+	glm::vec3 lightColor = glm::vec3(1.0); //White light
+	glm::vec3 ambientColor = glm::vec3(0.3, 0.4, 0.46);
+}light;
+
+float minBias = 0.005f;
+float maxBias = 0.015f;
+
+int main() {
+	GLFWwindow* window = initWindow("Assignment 5", screenWidth, screenHeight);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	GLuint rockTexture = ew::loadTexture("assets/Rock_Color.jpg");
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Shader postProcessShader = ew::Shader("assets/postProcess.vert", "assets/postProcess.frag");
+	ew::Shader depthOnlyShader = ew::Shader("assets/depthOnly.vert", "assets/depthOnly.frag");
+	ew::Model monkeyModel = ew::Model("assets/suzanne.obj");
+	ew::Transform monkeyTransform;
+
+	ew::Mesh planeMesh = ew::Mesh(ew::createPlane(10, 10, 5));
+	ew::Transform planeTransform;
+	planeTransform.position = glm::vec3(0.0f, -2.0f, 0.0f);
+	
+	//Main Camera
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f); //Look at center of the scene
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f; //Vertical field of view, in degrees
+
+	//Shadow Camera
+	shadowCamera.target = glm::vec3(0.0f, 0.0f, 0.0f);
+	shadowCamera.orthographic = true;
+	shadowCamera.orthoHeight = 15.0f;
+	shadowCamera.nearPlane = 0.01f;
+	shadowCamera.farPlane = 30.0f;
+	shadowCamera.aspectRatio = 1.0f;
+
+	//Create Framebuffer and shadow map
+	ns::Framebuffer framebuffer = ns::createFramebuffer(screenWidth, screenHeight, GL_RGB);
+	shadowMap = ns::createShadowMap(shadowMapWidth, shadowMapHeight);
+
+	unsigned int dummyVAO;
+	glCreateVertexArrays(1, &dummyVAO);
+	
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK); //Back face culling
+	glEnable(GL_DEPTH_TEST); //Depth testing
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - prevFrameTime;
+		prevFrameTime = time;
+
+		//RENDER
+		//Shadow Map
+		glCullFace(GL_FRONT);
+		glBindFramebuffer(GL_FRAMEBUFFER, shadowMap.fbo);
+		glViewport(0, 0, shadowMapWidth, shadowMapHeight);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		
+		shadowCamera.position = (shadowCamera.target - glm::normalize(light.lightDirection)) * 5.0f;
+		depthOnlyShader.use();
+		depthOnlyShader.setMat4("_ViewProjection", shadowCamera.projectionMatrix() * shadowCamera.viewMatrix());
+		glCullFace(GL_BACK);
+		depthOnlyShader.setMat4("_Model", monkeyTransform.modelMatrix());
+		monkeyModel.draw();
+		depthOnlyShader.setMat4("_Model", planeTransform.modelMatrix());
+		planeMesh.draw();
+
+		//Offscreen Framebuffer
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.fbo);
+		glViewport(0, 0, screenWidth, screenHeight);
+		glClearColor(0.6f,0.8f,0.92f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Camera Controller
+		cameraController.move(window, &camera, deltaTime);
+
+		//Rotate model around Y axis
+		monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
+		//Bind textures to texture units
+		glBindTextureUnit(0, rockTexture);
+		glBindTextureUnit(1, shadowMap.depthMap);
+
+		shader.use();
+		shader.setInt("_MainTex", 0); //Make "_MainTex" sampler2D sample from the 2D texture bound to unit 0
+		shader.setMat4("_Model", monkeyTransform.modelMatrix());
+		shader.setMat4("_ViewProjection", camera.projectionMatrix() * camera.viewMatrix());
+		shader.setVec3("_EyePos", camera.position);
+		shader.setMat4("_LightViewProj", shadowCamera.projectionMatrix() * shadowCamera.viewMatrix());
+		shader.setInt("_ShadowMap", 1);
+		//Light
+		shader.setVec3("_Light.LightDirection", light.lightDirection);
+		shader.setVec3("_Light.LightColor", light.lightColor);
+		shader.setVec3("_Light.AmbientColor", light.ambientColor);
+		shader.setFloat("_MinBias", minBias);
+		shader.setFloat("_MaxBias", maxBias);
+		//Material 
+		shader.setFloat("_Material.Ka", material.Ka);
+		shader.setFloat("_Material.Kd", material.Kd);
+		shader.setFloat("_Material.Ks", material.Ks);
+		shader.setFloat("_Material.Shininess", material.Shininess);
+		monkeyModel.draw(); //Draws the monkey model using current shader
+
+		shader.setMat4("_Model", planeTransform.modelMatrix());
+		planeMesh.draw();
+
+		//Scene
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		postProcessShader.use();
+		glBindTextureUnit(0, framebuffer.colorBuffer[0]);
+		glBindVertexArray(dummyVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		drawUI();
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
+	camera->position = glm::vec3(0, 0, 5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+}
+
+void drawUI() {
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Settings");
+	if (ImGui::Button("Reset Camera")) {
+		resetCamera(&camera, &cameraController);
+	}
+	if (ImGui::CollapsingHeader("Material")) {
+		ImGui::SliderFloat("AmbientK", &material.Ka, 0.0f, 1.0f);
+		ImGui::SliderFloat("DiffuseK", &material.Kd, 0.0f, 1.0f);
+		ImGui::SliderFloat("SpecularK", &material.Ks, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shininess", &material.Shininess, 2.0f, 1024.0f);
+	}
+	if (ImGui::CollapsingHeader("Light")) {
+		ImGui::SliderFloat3("Direction", &light.lightDirection.x, -1.0f, 1.0f);
+		ImGui::SliderFloat("Min Bias", &minBias, 0.001f, 0.05f);
+		ImGui::SliderFloat("Max Bias", &maxBias, 0.001f, 0.05f);
+	}
+	ImGui::End();
+
+	ImGui::Begin("Shadow Map");
+	ImGui::BeginChild("Shadow Map");
+	ImVec2 windowSize = ImGui::GetWindowSize();
+	ImGui::Image((ImTextureID)shadowMap.depthMap, windowSize, ImVec2(0, 1), ImVec2(1, 0));
+	ImGui::EndChild();
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+}
+
+/// <summary>
+/// Initializes GLFW, GLAD, and IMGUI
+/// </summary>
+/// <param name="title">Window title</param>
+/// <param name="width">Window width</param>
+/// <param name="height">Window height</param>
+/// <returns>Returns window handle on success or null on fail</returns>
+GLFWwindow* initWindow(const char* title, int width, int height) {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return nullptr;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	return window;
+}
+
